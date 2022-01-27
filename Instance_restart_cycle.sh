8ad9aee382482c2f6f6fc0fcd1fda68da8f1d012
Dan Burdetsky <34523615+ko3ak81@users.noreply.github.com>
2022-01-27 11:37:36 +0100

    Create Instance_restart_cycle.sh

---
+++ Instance_restart_cycle.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+i=0
+while [ $i -le 10 ];
+do
+
+        echo "Cycle number: $i"
+        state=$(aws ec2 describe-instances --instance-id i-0c88908dcc921268d | jq '.Reservations[0].Instances[0].State.Name' | tr -d '"')
+
+        if [[ $state == *"running"* ]]; then
+          date
+          sleep 20
+          echo "Stopping the instance"
+          aws ec2 stop-instances --instance-ids i-0c88908dcc111268d
+          i=$(($i+1))
+
+        elif [[ $state == *"stopped"* ]]; then
+          date
+          echo "Starting the instance"
+          aws ec2 start-instances --instance-ids i-0c88908dcc111268d
+          i=$(($i+1))
+
+        else
+          sleep 5
+
+        fi
+
+done
+
